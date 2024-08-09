e4ddc406591941ab0854068b3e16034521ed389e
Yauhen Vavilkin <yauhen_vavilkin@epam.com>
2024-08-09 16:02:53 +0400

    adjust tests

--- src/test/java/org/folio/md/validator/service/impl/EndpointRequiredPermissionsValidatorTest.java
+++ src/test/java/org/folio/md/validator/service/impl/EndpointRequiredPermissionsValidatorTest.java
@@ -6,52 +6,27 @@ import static org.folio.md.validator.support.TestUtils.readModuleDescriptor;
 import org.folio.md.validator.model.ValidationContext;
 import org.folio.md.validator.service.Validator;
 import org.folio.md.validator.support.UnitTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @UnitTest
 class EndpointRequiredPermissionsValidatorTest {
 
   private final Validator validator = new EndpointRequiredPermissionsValidator();
 
-  @Test
-  void validate_positive() {
-    var moduleDescriptor = readModuleDescriptor("json/endpoint-required-perms/md-valid.json");
+  @ParameterizedTest
+  @CsvSource({
+    "json/endpoint-required-perms/md-valid.json, 0",
+    "json/endpoint-required-perms/md-non-valid-multiple-permissions.json, 1",
+    "json/endpoint-required-perms/md-non-valid-protected-by-sub-permission.json, 1",
+    "json/endpoint-required-perms/md-non-valid-multiple-and-sub-permissions.json, 2"
+  })
+  void validate(String filePath, int expectedErrorCount) {
+    var moduleDescriptor = readModuleDescriptor(filePath);
     var ctx = new ValidationContext(moduleDescriptor);
 
     validator.validate(ctx);
 
-    assertThat(ctx.getErrorParameters()).isEmpty();
-  }
-
-  @Test
-  void validate_negative_multiplePermissions() {
-    var moduleDescriptor = readModuleDescriptor("json/endpoint-required-perms/md-non-valid-multiple-permissions.json");
-    var ctx = new ValidationContext(moduleDescriptor);
-
-    validator.validate(ctx);
-
-    assertThat(ctx.getErrorParameters()).hasSize(1);
-  }
-
-  @Test
-  void validate_negative_protectedBySubPermission() {
-    var moduleDescriptor = readModuleDescriptor(
-      "json/endpoint-required-perms/md-non-valid-protected-by-sub-permission.json");
-    var ctx = new ValidationContext(moduleDescriptor);
-
-    validator.validate(ctx);
-
-    assertThat(ctx.getErrorParameters()).hasSize(1);
-  }
-
-  @Test
-  void validate_negative_protectedByMultipleAndSubPermissions() {
-    var moduleDescriptor = readModuleDescriptor(
-      "json/endpoint-required-perms/md-non-valid-multiple-and-sub-permissions.json");
-    var ctx = new ValidationContext(moduleDescriptor);
-
-    validator.validate(ctx);
-
-    assertThat(ctx.getErrorParameters()).hasSize(2);
+    assertThat(ctx.getErrorParameters()).hasSize(expectedErrorCount);
   }
 }
