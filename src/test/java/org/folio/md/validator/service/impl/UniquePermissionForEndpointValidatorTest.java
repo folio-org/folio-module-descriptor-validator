154ea9d7d3316029762fa97ad49df078f1d7a657
Aliaksei Harbuz <Aliaksei_Harbuz@epam.com>
2026-09-21 14:33:26 +0300

    MVNMDVAL-15 - validate_negative_wildcardWithDuplicateNamedPermission

--- src/test/java/org/folio/md/validator/service/impl/UniquePermissionForEndpointValidatorTest.java
+++ src/test/java/org/folio/md/validator/service/impl/UniquePermissionForEndpointValidatorTest.java
@@ -42,4 +42,15 @@ class UniquePermissionForEndpointValidatorTest {
 
     assertThat(ctx.getErrorParameters()).isEmpty();
   }
+
+  @Test
+  void validate_negative_wildcardWithDuplicateNamedPermission() {
+    var moduleDescriptor = readModuleDescriptor(
+      "json/unique-permission/md-wildcard-with-duplicate-named-permission.json");
+    var ctx = new ValidationContext(moduleDescriptor);
+
+    validator.validate(ctx);
+
+    assertThat(ctx.getErrorParameters()).hasSize(1);
+  }
 }
